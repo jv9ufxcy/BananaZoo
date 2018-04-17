c1d8d6ed1f0e678db1741488e4ed2233c1d389e2
JackStrider <rossi.william@gmail.com>
2018-04-16 23:44:28 -0500

    Messed around with random for temprature controlls

---
+++ BananaZoo/Assets/Scripts/Habitat1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Habitat1 : MonoBehaviour 
+{
+    Random r = new Random();
+    int habitat1Random = r.Next(31);
+    
+    Text labelText;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+        labelText.text = "Habitat 1 temp = " + r.Next(31,110);
+    }
+}
---
+++ BananaZoo/Assets/Scripts/TempratureControllerGrand.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempratureControllerGrand : MonoBehaviour 
+{
+
+    public bool habitat1check = false;
+    public bool habitat2check = false;
+    public bool habitat3check = false;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (habitat1check == true && habitat2check == true && habitat3check == true)
+        {
+            Debug.Log("eyo");
+        }
+	}
+}
